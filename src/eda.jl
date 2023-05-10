a7c7794b67a9ed9d2fb0919757462f4f79d8d339
indymnv <indy.mnv@gmail.com>
2023-05-10 18:09:47 +0900

    clean file

--- src/eda.jl
+++ src/eda.jl
@@ -4,25 +4,18 @@ using CSV
 using StatsBase
 
 # read the file into a dataframe
-df = DataFrame(CSV.File("./Spotify_Youtube.csv"))
+ df = DataFrame(CSV.File("./Spotify_Youtube.csv"))
 
 # create a function to perform feature engineering with one hot encoder
-scatter(df.Energy, df.Loudness)
+ scatter(df.Energy, df.Loudness)
 
-#histogram with different columns
-plot([histogram(df[:, col],label = col, bins = 20 ) for col in ["Danceability",
-		"Energy",
-		"Key",
-		"Loudness",
-		"Speechiness",
-		"Acousticness",
-		"Instrumentalness",
-		"Liveness",
-		"Valence",
-		"Tempo",
-		"Duration_ms",
-		"Views",
-		"Likes",
-		"Comments",
-]]...)
+ plot([histogram(df[:, col],label = col, bins = 10 ) for col in ["Danceability", "Energy","Key","Loudness","Speechiness","Acousticness","Instrumentalness","Liveness","Valence","Tempo","Duration_ms","Views","Likes","Comments"]]...)
+
+# crete a pairplot function will receive a dataframe and will create a plot with several scatterplot to cross information
+
+function pairplot(df::DataFrame)
+		
+end
+
+
 

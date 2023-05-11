a23f0c96d3091cb60594e34cf2c948dc18cbe60a
indymnv <indy.mnv@gmail.com>
2023-05-11 23:17:15 +0900

    new viz with statsplots

--- src/eda.jl
+++ src/eda.jl
@@ -2,6 +2,8 @@ using DataFrames
 using Plots
 using CSV
 using StatsBase
+using StatsPlots
+using Statistics
 
 # read the file into a dataframe
  df = DataFrame(CSV.File("./Spotify_Youtube.csv"))
@@ -12,10 +14,11 @@ using StatsBase
  plot([histogram(df[:, col],label = col, bins = 10 ) for col in ["Danceability", "Energy","Key","Loudness","Speechiness","Acousticness","Instrumentalness","Liveness","Valence","Tempo","Duration_ms","Views","Likes","Comments"]]...)
 
 # crete a pairplot function will receive a dataframe and will create a plot with several scatterplot to cross information
-
-function pairplot(df::DataFrame)
-		
-end
+df_num = dropmissing(df[:,["Danceability", "Energy","Key","Loudness","Speechiness","Acousticness","Instrumentalness","Liveness","Valence","Tempo","Duration_ms","Views","Likes","Comments"]])
 
 
+gr(size = (1200, 1000))
+@df df_num corrplot(cols(1:9), grid = true)
 
+
+@df df_num cornerplot(cols(1:5),)
